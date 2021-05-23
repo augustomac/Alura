306699eafb84f23260265055d63411febd13af0f
Augusto <augustoludovice@gmail.com>
2021-05-23 13:49:21 -0400

    String

---
+++ Dart/primeirospassos/variaveisString.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  int ano = 2021;
+  String texto = "Augusto Ludovice Machado da silva";
+  print("Meu nome é $texto e comecei a aprender Dart em 2021");
+  ano = ano++;
+  print(ano);
+}
