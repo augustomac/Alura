52bccf78a48dcab370e1a8d8c9a8aa6d0a9c0ece
Augusto <augustoludovice@gmail.com>
2021-05-23 12:38:41 -0400

    inteiros

---
+++ Dart/primeirospassos/variaveisInteiras.dart
@@ -0,0 +1,7 @@
+void main() {
+  int idade = 25;
+  int idade_irmao = idade + 2;
+  int idade_avo = (idade + idade_irmao) * 3;
+  print(
+      "eu tenho $idade anos, meu irmao tem $idade_irmao e meu irmão tem $idade_avo anos.");
+}
