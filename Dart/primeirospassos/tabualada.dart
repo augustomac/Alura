bc68d22132f2d1d2a4ec34ad522726e9f20c430d
Augusto <augustoludovice@gmail.com>
2021-05-26 23:45:59 -0400

    classes

--- Dart/primeirospassos/tabualada.dart
+++ Dart/primeirospassos/tabualada.dart
@@ -6,13 +6,13 @@ void main() {
   while (condicao) {
     print(
         "====Entre com o valor para calcular a tabuado ou digite sair para terminar.");
-    String? entrada = stdin.readLineSync();
+    late String entrada = stdin.readLineSync();
     if (entrada == "sair") {
       condicao = false;
-      print("Programa encerrado");
+      print("Programa encerrado")
     } else {
       print("\x1B[2J\x1B[0;0H");
-      int numero = int.parse(entrada!);
+      int numero = int.parse(entrada);
       for (int contador = 1; contador <= 10; contador++) {
         int resultado = numero * contador;
         print("$numero * $contador = $resultado");
